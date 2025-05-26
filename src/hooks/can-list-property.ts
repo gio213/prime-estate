6edf0ef76f2e6dde9a043b7709d5046d3204fa52
Giorgi Patsia <gio.patsia@gmail.com>
2025-05-26 22:02:25 +0200

    fixed deploiment bug

--- src/hooks/can-list-property.ts
+++ src/hooks/can-list-property.ts
@@ -4,10 +4,7 @@ import { cache } from "react";
 export const getUserCredit = cache(async () => {
   const userData = await get_current_user();
 
-  if (!userData) {
-    return null;
-  }
-  if (userData.credit === 0) {
+  if (userData?.credit === 0) {
     return false;
   } else {
     return true;
